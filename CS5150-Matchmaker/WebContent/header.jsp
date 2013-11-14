8a733901667b1448a6733827b4ff19eef2da5df6
Jasmine Ahuja <jka63@cornell.edu>
2013-11-13 22:48:40 -0500

    Combined profile and edit-profile pages into one - bringing the new pages into frontend branch

--- CS5150-Matchmaker/WebContent/header.jsp
+++ CS5150-Matchmaker/WebContent/header.jsp
@@ -4,34 +4,34 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<link rel="stylesheet" type="text/css" href="css/jPages.css">
-	<link rel="stylesheet" type="text/css" href="css/autoSuggest.css">
-	<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
-	<script src="http://code.jquery.com/jquery-migrate-1.2.1.js"></script>
-	<script src="js/script.js" type="text/javascript"></script>
-	<script src="js/jquery.tablesorter.min.js" type="text/javascript"></script>
-	<script src="js/jquery.tablesorter.pager.js" type="text/javascript"></script>
-	<script src="js/jquery.autoSuggest.js" type="text/javascript"></script>
-	<title>Matchmaker - Student Profile</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="stylesheet" type="text/css" href="css/jPages.css">
+        <link rel="stylesheet" type="text/css" href="css/autoSuggest.css">
+        <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+        <script src="http://code.jquery.com/jquery-migrate-1.2.1.js"></script>
+        <script src="js/script.js" type="text/javascript"></script>
+        <script src="js/jquery.tablesorter.min.js" type="text/javascript"></script>
+        <script src="js/jquery.tablesorter.pager.js" type="text/javascript"></script>
+        <script src="js/jquery.autoSuggest.js" type="text/javascript"></script>
+        <title>Matchmaker - Student Profile</title>
 </head>
     <!--[if IE 7]>         <body class="lt-ie10 lt-ie9 lt-ie8"> <![endif]-->
     <!--[if IE 8]>         <body class="lt-ie10 lt-ie9"> <![endif]-->
     <!--[if IE 9]>         <body class="lt-ie10"> <![endif]-->
     <!--[if gt IE 9]><!--> <body> <!--<![endif]-->
-	<div id="top-bar" class="clearfix">
-		<ul class="login-nav">
+        <div id="top-bar" class="clearfix">
+                <ul class="login-nav">
 
-		<% if(request.getParameter("stud_or_prof").equals("stud")){ 
+                <% if(request.getParameter("stud_or_prof").equals("stud")){ 
 
-				Student s = StudentController.getStudentByNetID("lr437");%>
+                                Student s = StudentController.getStudentByNetID("lr437");%>
 
-					<li>Welcome, <%=s.getName() %></li>
+                                        <li>Welcome, <%=s.getName() %></li>
 
-		<% } else if(request.getParameter("stud_or_prof").equals("researcher")){ %>
+                <% } else if(request.getParameter("stud_or_prof").equals("researcher")){ %>
 
-		<%Researcher r = ResearcherController.getResearcherByNetID("tm123"); %>
+                <%Researcher r = ResearcherController.getResearcherByNetID("tm123"); %>
 
 <li>Welcome, <%=r.getName() %></li>
 
@@ -40,39 +40,31 @@
 <li class="login-link"><a href="#">sign out</a></li>
 
 </ul>
-	</div>
-	<div class="page">
-		<div class="wrapper">
-			<div id="container" class="clearfix">
-				<div class="sidebar">
-					<p class="notifications">Notifications</p>
-					<div id="sidenav">
-						<ul>
-							<jsp:include page="sidebar.jsp">
-								<jsp:param name="sidebar_type" value="<%= request.getParameter(\"sidebar_type\") %>" />
-							    <jsp:param name="sidebar_selected" value="<%= request.getParameter(\"sidebar_selected\") %>" />
-							</jsp:include>
-						</ul>
-					</div>
-				</div>
-				<div class="main">
-					<ul class="tabrow">
-						<% if(request.getParameter("stud_or_prof").equals("stud")){ %>
-							<li <% if(request.getParameter("top_selected").equals("profile")){ %>class="selected"<% } %>>
-								<a href="profile.jsp">My Profile</a>
-							</li>
-							<li <% if(request.getParameter("top_selected").equals("project")){ %>class="selected"<% } %>>
-								<a href="student-projects.jsp">Projects</a>
-							</li>
-						<% } else if(request.getParameter("stud_or_prof").equals("researcher")){ %>
-							<li <% if(request.getParameter("top_selected").equals("profile")){ %>class="selected"<% } %>>
-								<a href="researcher-profile.jsp">My Profile</a>
-							</li>
-							<li <% if(request.getParameter("top_selected").equals("project")){ %>class="selected"<% } %>>
-								<a href="researcher-projects.jsp">My Projects</a>
-							</li>
-							<li <% if(request.getParameter("top_selected").equals("students")){ %>class="selected"<% } %>>
-								<a href="invite-students.jsp">Students</a>
-							</li>
-						<% } %>
-					</ul>
+        </div>
+        <div class="page">
+                <div class="wrapper">
+                        <div id="container" class="clearfix">
+                                <div class="sidebar">
+                                        <p class="notifications">Notifications</p>
+                                </div>
+                                <div class="main">
+                                        <ul class="tabrow">
+                                                <% if(request.getParameter("stud_or_prof").equals("stud")){ %>
+                                                        <li <% if(request.getParameter("top_selected").equals("profile")){ %>class="selected"<% } %>>
+                                                                <a href="profile.jsp">My Profile</a>
+                                                        </li>
+                                                        <li <% if(request.getParameter("top_selected").equals("project")){ %>class="selected"<% } %>>
+                                                                <a href="student-projects.jsp">Projects</a>
+                                                        </li>
+                                                <% } else if(request.getParameter("stud_or_prof").equals("researcher")){ %>
+                                                        <li <% if(request.getParameter("top_selected").equals("profile")){ %>class="selected"<% } %>>
+                                                                <a href="researcher-profile.jsp">My Profile</a>
+                                                        </li>
+                                                        <li <% if(request.getParameter("top_selected").equals("project")){ %>class="selected"<% } %>>
+                                                                <a href="researcher-projects.jsp">My Projects</a>
+                                                        </li>
+                                                        <li <% if(request.getParameter("top_selected").equals("students")){ %>class="selected"<% } %>>
+                                                                <a href="invite-students.jsp">Students</a>
+                                                        </li>
+                                                <% } %>
+                                        </ul>
--- CS5150-Matchmaker/WebContent/js/script.js
+++ CS5150-Matchmaker/WebContent/js/script.js
@@ -1,16 +1,16 @@
 function initSideHeight()
 {
-	var sidebar = $(".sidebar #sidenav");
-	if(sidebar.length == 0) {return; }
-	if($(".main .content").height() > sidebar.height())
-		sidebar.height($(".main .content").height());
-	else
-		$(".main .content").height(sidebar.height());
+        var sidebar = $(".sidebar #sidenav");
+        if(sidebar.length == 0) {return; }
+        if($(".main .content").height() > sidebar.height())
+                sidebar.height($(".main .content").height());
+        else
+                $(".main .content").height(sidebar.height());
 }
 
 function doPagination() {
-	var pag_holder = $("ul.holder");
-	if(pag_holder.length == 0) {return;}
+        var pag_holder = $("ul.holder");
+        if(pag_holder.length == 0) {return;}
     pag_holder.jPages({
         containerID: "project-list-pagination",
         perPage: 50
@@ -18,160 +18,173 @@ function doPagination() {
 }
 
 function initAutosuggest()
-{	
-	var majorInput = $(".info input[name=major], #filter-list input[name=filter-major]");
-	var minorInput = $(".info input[name=minor]");
-	var collegeInput = $(".info input[name=school]");
-	var skillsInput = $(".application-info input[name=skills], .info input[name=skills], #filter-list input[name=filter-skill]");
-	var researchInput = $(".application-info input[name=research-area], .info input[name=research-area], #filter-list input[name=filter-interest]");
-	
-	if(majorInput.length){
-		majorInput.autoSuggest(majorData.items, {selectedItemProp: "name", searchObjProps: "name", startText: "",asHtmlID: "major"});
-	}
-	if(minorInput.length){
-		minorInput.autoSuggest(minorData.items, {selectedItemProp: "name", searchObjProps: "name", startText: "",asHtmlID: "minor"});
-	}
-	if(collegeInput.length){
-		collegeInput.autoSuggest(collegeData.items, {selectedItemProp: "name", searchObjProps: "name", startText: "",asHtmlID: "college"});
-	}
-	if(skillsInput.length){
-		skillsInput.autoSuggest(skillsData.items, {selectedItemProp: "name", searchObjProps: "name", startText: "",asHtmlID: "skills"});
-	}
-	if(researchInput.length){
-		researchInput.autoSuggest(interestData.items, {selectedItemProp: "name", searchObjProps: "name", startText: "",asHtmlID: "research"});
-	}
-	
+{        
+        var majorInput = $(".info input[name=major], #filter-list input[name=filter-major]");
+        var minorInput = $(".info input[name=minor]");
+        var collegeInput = $(".info input[name=school]");
+        var skillsInput = $(".application-info input[name=skills], .info input[name=skills], #filter-list input[name=filter-skill]");
+        var researchInput = $(".application-info input[name=research-area], .info input[name=research_interests], #filter-list input[name=filter-interest]");
+        
+        if(majorInput.length){
+                majorInput.autoSuggest(majorData.items, {selectedItemProp: "name", searchObjProps: "name", startText: "",asHtmlID: "major"});
+        }
+        if(minorInput.length){
+                minorInput.autoSuggest(minorData.items, {selectedItemProp: "name", searchObjProps: "name", startText: "",asHtmlID: "minor"});
+        }
+        if(collegeInput.length){
+                collegeInput.autoSuggest(collegeData.items, {selectedItemProp: "name", searchObjProps: "name", startText: "",asHtmlID: "college"});
+        }
+        if(skillsInput.length){
+                skillsInput.autoSuggest(skillsData.items, {selectedItemProp: "name", searchObjProps: "name", startText: "",asHtmlID: "skills"});
+        }
+        if(researchInput.length){
+                researchInput.autoSuggest(interestData.items, {selectedItemProp: "name", searchObjProps: "name", startText: "",asHtmlID: "research"});
+        }
+        
+}
+
+function EditField()
+{
+        var edit_btn = $("a.edit-btn");
+        if(edit_btn.length == 0) { return; }
+        edit_btn.click(function(){
+                $(this).parent().fadeOut("slow", function(){
+                        $(this).next().fadeIn();
+                });
+                
+                return false;
+        });
 }
 
 function handleFilterText(){
-	var formEl = $("#add-new-filters");
-	var filters = $(".filters");
-	if(formEl.length == 0) {return; }
-	formEl.submit(function(){
-		var inputFields = $("li input[type=text]", this);
-		
-		inputFields.each(function(index, el){
-			if($(el).val().length != 0 && filters.length != 0){
-				filters.append('<input type="checkbox" name="' + $(el).attr("name") + '">' + $(el).val());
-				filters.last().click(handleSingleCheckbox);
-				$(el).val('');
-			}
-		});
-		return false;
-	});
+        var formEl = $("#add-new-filters");
+        var filters = $(".filters");
+        if(formEl.length == 0) {return; }
+        formEl.submit(function(){
+                var inputFields = $("li input[type=text]", this);
+                
+                inputFields.each(function(index, el){
+                        if($(el).val().length != 0 && filters.length != 0){
+                                filters.append('<input type="checkbox" name="' + $(el).attr("name") + '">' + $(el).val());
+                                filters.last().click(handleSingleCheckbox);
+                                $(el).val('');
+                        }
+                });
+                return false;
+        });
 }
 
 function handleFilterCheckboxes(){
-	var checkboxes = $(".filters input[type=checkbox]");
-	if(checkboxes.length == 0) {return;}
-	checkboxes.each(function(index, el){
-		$(el).click(handleSingleCheckbox);
-	});
+        var checkboxes = $(".filters input[type=checkbox]");
+        if(checkboxes.length == 0) {return;}
+        checkboxes.each(function(index, el){
+                $(el).click(handleSingleCheckbox);
+        });
 }
 
 function handleSingleCheckbox(){
-	var items = $(".project-list li");
-	console.log("hiding " + items.length + " items");
-	items.hide();
-	var rand1 = Math.floor((Math.random()*500)+1);
-	var rand2 = Math.floor((Math.random()*500)+rand1);
-	console.log("rand1=" + rand1 + " rand2=" + rand2);
-	items.slice(rand1, rand2).show();
-	doPagination();
-	initSideHeight();
+        var items = $(".project-list li");
+        console.log("hiding " + items.length + " items");
+        items.hide();
+        var rand1 = Math.floor((Math.random()*500)+1);
+        var rand2 = Math.floor((Math.random()*500)+rand1);
+        console.log("rand1=" + rand1 + " rand2=" + rand2);
+        items.slice(rand1, rand2).show();
+        doPagination();
+        initSideHeight();
 }
 
 function handleAddCourse(){
-	var addbtn = $("#add-course");
-	if(addbtn.length == 0) {return;}
-	var courseTable = $("#profile-courses");
-	var tableRowHtml = courseTable.find("tr").html();
-	addbtn.click(function(){
-		courseTable.append("<tr>"+tableRowHtml+"</tr>");
-		initSideHeight();
-	});
+        var addbtn = $("#add-course");
+        if(addbtn.length == 0) {return;}
+        var courseTable = $("#profile-courses");
+        var tableRowHtml = courseTable.find("tr").html();
+        addbtn.click(function(){
+                courseTable.append("<tr>"+tableRowHtml+"</tr>");
+                initSideHeight();
+        });
 }
 
 function IsEmail(email) {
-	  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-	  return regex.test(email);
+          var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+          return regex.test(email);
 }
 
 function validateFormSubmit()
 {
-	var formEl = $("form");
-	if(formEl.length == 0){return;}
-	formEl.submit(function(e){
-		var emailEl = $("input[name=email]");
-		if(emailEl.length){
-			if(!IsEmail(emailEl.val())){
-				e.preventDefault();
-				return false;
-			}
-		}
-		var textElReq = $(".required input[type=text]").not($(".as-selections input[type=text]"));
-		var hiddenElReq = $(".required .as-selections input[type=hidden]");
-		if(textElReq.length){
-			textElReq.each(function(index, el){
-				console.log($(el).parent().html());
-				if($(el).val().length == 0){
-					e.preventDefault();
-					return false;
-				}
-			});
-		}
-		if(hiddenElReq.length){
-			hiddenElReq.each(function(index, el){
-				console.log($(el).parent().html());
-				if($(el).val().length == 0){
-					e.preventDefault();
-					return false;
-				}
-			});
-		}
-	});
+        var formEl = $("form");
+        if(formEl.length == 0){return;}
+        formEl.submit(function(e){
+                var emailEl = $("input[name=email]");
+                if(emailEl.length){
+                        if(!IsEmail(emailEl.val())){
+                                e.preventDefault();
+                                return false;
+                        }
+                }
+                var textElReq = $(".required input[type=text]").not($(".as-selections input[type=text]"));
+                var hiddenElReq = $(".required .as-selections input[type=hidden]");
+                if(textElReq.length){
+                        textElReq.each(function(index, el){
+                                console.log($(el).parent().html());
+                                if($(el).val().length == 0){
+                                        e.preventDefault();
+                                        return false;
+                                }
+                        });
+                }
+                if(hiddenElReq.length){
+                        hiddenElReq.each(function(index, el){
+                                console.log($(el).parent().html());
+                                if($(el).val().length == 0){
+                                        e.preventDefault();
+                                        return false;
+                                }
+                        });
+                }
+        });
 }
 
 function hideProject()
 {
-	var delButton = $(".project-info .delete");
-	if(delButton.length == 0) {return;}
-	delButton.click(function(){
-		$(this).parent().parent().animate({
-		    opacity: 0,
-		    height: 'toggle'
-		  }, 1000);
-		doPagination();
-		initSideHeight();
-	});
+        var delButton = $(".project-info .delete");
+        if(delButton.length == 0) {return;}
+        delButton.click(function(){
+                $(this).parent().parent().animate({
+                    opacity: 0,
+                    height: 'toggle'
+                  }, 1000);
+                doPagination();
+                initSideHeight();
+        });
 }
 
 function filterAll()
 {
-	var filterLink = $(".filters .filter-all");
-	if(filterLink.length == 0){return;}
-	var checkboxes = $(".filters input[type=checkbox]");
-	filterLink.click(function(){
-		checkboxes.each(function(idx, el){
-			$(el).prop("checked", true);
-		});
-		handleSingleCheckbox();
-	});
+        var filterLink = $(".filters .filter-all");
+        if(filterLink.length == 0){return;}
+        var checkboxes = $(".filters input[type=checkbox]");
+        filterLink.click(function(){
+                checkboxes.each(function(idx, el){
+                        $(el).prop("checked", true);
+                });
+                handleSingleCheckbox();
+        });
 }
 
 function sortTable()
 {
-	$(".project-list").tablesorter()
-	.tablesorterPager({container: $("#pager"), positionFixed: false});
+        $(".project-list").tablesorter()
+        .tablesorterPager({container: $("#pager"), positionFixed: false});
 }
 
 $(document).ready(function(){
-	initAutosuggest();
-	sortTable();
-	handleAddCourse();
-	validateFormSubmit();
-	hideProject();
-	filterAll();
-	initSideHeight();
+        initAutosuggest();
+        sortTable();
+        EditField();
+        handleAddCourse();
+        validateFormSubmit();
+        hideProject();
+        filterAll();
+        initSideHeight();
 });
-
