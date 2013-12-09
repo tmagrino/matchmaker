3d1007a8fb6b73ad913ca562aa5f0fa4c7c0cc97
lrmneves <lrmneves@poli.ufrj.br>
2013-12-09 01:25:53 -0400

    Took tutorial function out of email.java

--- CS5150-Matchmaker/src/model/Email.java
+++ CS5150-Matchmaker/src/model/Email.java
@@ -58,35 +58,6 @@ public class Email {
         }
     }
     
-    private static void sendFromGMail(String to, String subject, String body) {
-        Properties props = System.getProperties();
-        String host = "smtp.gmail.com";
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", host);
-        props.put("mail.smtp.user", from);
-        props.put("mail.smtp.password", pass);
-        props.put("mail.smtp.port", "587");
-        props.put("mail.smtp.auth", "true");
-
-        Session session = Session.getDefaultInstance(props);
-        MimeMessage message = new MimeMessage(session);
-
-        try {
-            message.setFrom(new InternetAddress(from));
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-
-            message.setSubject(subject);
-            message.setText(body);
-            Transport transport = session.getTransport("smtp");
-            transport.connect(host, from, pass);
-            transport.sendMessage(message, message.getAllRecipients());
-            transport.close();
-        }
-        catch (AddressException ae) {
-            ae.printStackTrace();
-        }
-        catch (MessagingException me) {
-            me.printStackTrace();
-        }
-    }
+    
+  
 }
